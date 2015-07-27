da6acff5ec4834a0c3e938485c6fa024af1712c1
yfor <377900058@qq.com>
2015-07-27 14:06:36 +0800

    emacs
    fins the amzing of emacs

---
+++ codeForReadBook/SICP/greet.py
@@ -0,0 +1,13 @@
+print "Ctr-x-s; save"
+print "Ctr-x o change window"
+print "Ctr-c-c run buffer"
+print "HELLO"
+print "ctr-c-z open python"
+print "move ctr b f p n ;m "
+
+my_name = "wq"
+def greet():
+    print my_name
+    print "python"
+
+print "Ctr + v: next page;Alt + v pre page"

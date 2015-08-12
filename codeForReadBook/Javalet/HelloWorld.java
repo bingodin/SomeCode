a1da4e44d2b5a18d76a9f259798d861330805755
yfor <377900058@qq.com>
2015-08-12 18:16:30 +0800

    github may use UFT-8
    上传

--- codeForReadBook/Javalet/HelloWorld.java
+++ codeForReadBook/Javalet/HelloWorld.java
@@ -1,8 +1,15 @@
-﻿public class HelloWorld{
+﻿
+/**
+韩顺平java lesson  1-2
+
+**/
+public class HelloWorld{
 	public static void main(String[] argv){
+	//install jdk set path javac HelloWold.java java HelloWold
 	System.out.println("hello world");//no space no upper no "1"
 	//------------------------------------------------------------------------------------
-	int a = 10;
+	//define ini assignment
+	int a = 10; 
 	int b = 20;
 	System.out.println(a+b);//var and op
 	//------------------------------------------------------------------------------------
@@ -13,10 +20,18 @@
 	0	0	0	0	0	0	0	0 	0
 	1	0	0	0	0	0	0	0 	-128 (本来是-0)
 	1	1	1	1	1	1	1	1	127 2^7-1
-	-128-127
+
 
 	**/
-	float f= 1.0f;//or this will need double
-	System.out.println(f);	
+	float f= 1.0f;//or this will need double  3.4 defalut is double
+	System.out.println(f);
+	//char
+	char c1='你';//2 byte
+	c1='a'+1; //'b'  有没有自动转换 字面值1的默认类型？
+	//'a'+10000000; will be error be careful 
+	System.out.println(c1);	
+	//byte short int long float double low to high
+	int fti  = (int )1.2;
+	System.out.println(fti);
 	}
 }

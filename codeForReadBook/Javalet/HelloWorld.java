f5b6bdfb6533c021cdb2913e34a2e4a2febf10a8
yfor <377900058@qq.com>
2015-08-12 17:38:08 +0800

    提交JAVALET
    编码，基础的java知识

---
+++ codeForReadBook/Javalet/HelloWorld.java
@@ -0,0 +1,22 @@
+﻿public class HelloWorld{
+	public static void main(String[] argv){
+	System.out.println("hello world");//no space no upper no "1"
+	//------------------------------------------------------------------------------------
+	int a = 10;
+	int b = 20;
+	System.out.println(a+b);//var and op
+	//------------------------------------------------------------------------------------
+	/**
+	The range of Byte
+	1	2	3	4	5	6	7	8
+
+	0	0	0	0	0	0	0	0 	0
+	1	0	0	0	0	0	0	0 	-128 (本来是-0)
+	1	1	1	1	1	1	1	1	127 2^7-1
+	-128-127
+
+	**/
+	float f= 1.0f;//or this will need double
+	System.out.println(f);	
+	}
+}

379c2ed7665649c871145c811eeeb2719ceda2b5
yfor <377900058@qq.com>
2015-08-22 01:10:34 +0800

    maven
    maventest

---
+++ maven/maventest/src/main/java/com/yfor/App.java
@@ -0,0 +1,13 @@
+package com.yfor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ maven/maventest/src/test/java/com/yfor/AppTest.java
@@ -0,0 +1,19 @@
+package com.companyname.bank;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+   
+{
+	@Test
+    public void testApp()
+    {
+	System.out.println("hi");
+
+        assertTrue( true );
+    }
+}

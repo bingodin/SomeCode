de48a2580017f4a7bee58ef6bb1d07945934238d
yfor <377900058@qq.com>
2016-01-04 21:22:00 +0800

    少说废话
    删除废话

--- misc/isdivablle3.js
+++ misc/isdivablle3.js
@@ -1,28 +1,27 @@
 //不使用除法和模运算计算一个数是否被三整除
 
 // 数字字符串的求和  "121" =>4
-function sumofA(a){
-sum=0;
-for(var i=0;i<a.length;i++){
-sum+=parseInt( a[i]
-)}
+function sumofA(a) {
+	sum = 0;
+	for (var i = 0; i < a.length; i++) {
+		sum += parseInt(a[i])
+	}
 
-return sum
+	return sum
 }
 
-
-function isd(n){
-if(n<10)
-{if(n==3||n==6||n==9)return true 
-else return false }
-else 
-{return  isd(
-sumofA(
-(n+'').split('')
-)
-)
+function isd(n) {
+	if (n < 10) {
+		if (n == 3 || n == 6 || n == 9)
+			return true else
+				return false
+	} else {
+		return isd(
+			sumofA(
+				(n + '').split('')))
+	}
 }
-}
- isd(20)
- isd(21)
- isd(210)
+isd(20)
+isd(21)
+isd(210)
+//开始google的一个题目,可以用小学知识解决，也是一本树上的习题
